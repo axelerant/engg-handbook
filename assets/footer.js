50e7f0dccec2e99a1bc86b3001366d32ac1bc4df
rathermuneer <rmmuneer06@gmail.com>
2022-11-09 13:57:59 +0530

    footer: Update footer theming.

--- assets/footer.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-  $(".menu-items-wrapper h2, .contact-info, .join-us, svg").on("click", function () {
-    $(this).toggleClass("active");
-    $(this).closest(".menu-wrapper").toggleClass("active-menu");
-  });
-});
